406623e0e4dde79cbcd2b35205af02b8d38a6c13
xujianxiang <584972029@qq.com>
2024-03-14 15:38:50 +0800

    chore: del unuse code

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,9 @@ import { TheFeature } from "@/components/kit/landing-page/TheFeature";
 import { TheHero } from "@/components/kit/landing-page/TheHero";
 import { ThePricing } from "@/components/kit/landing-page/ThePricing";
 
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
 
 export default async function Home() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
 
-  const { data: todos } = await supabase.from('todos').select()
-  
   return (
     <main className="flex min-h-screen flex-col items-start justify-center">
 
